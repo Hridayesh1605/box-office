ce52fb96eb654c3d421ffe0ccd716651ae557d38
Hridayesh16 <hridayeshmore16@gmail.com>
2021-07-17 19:52:21 +0530

    created styled components and added styled to search cards

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ActorCard from './ActorCard';
+
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 
 const ActorGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGrid>
       {data.map(({ person }) => (
         <ActorCard
           key={person.id}
-          id={person.id}
           name={person.name}
           country={person.country ? person.country.name : null}
           birthday={person.birthday}
@@ -17,7 +18,7 @@ const ActorGrid = ({ data }) => {
           image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
 
